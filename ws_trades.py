723a258f54398d2e12c27c3b202d498b97ed3530
jay chen <58091374+Jay-Trades@users.noreply.github.com>
2021-06-03 20:07:41 -0400

    Add files via upload

--- ws_trades.py
+++ ws_trades.py
@@ -23,9 +23,11 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-##parent folder import hack
+##parent folder import hack 
 import sys
 import os
+from datetime import datetime
+import pytz
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ##
 
@@ -33,6 +35,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #from bitso import Client, Listener
 from bitso import *
 
+utc=pytz.UTC
 
 class BasicBitsoListener(Listener):
     def on_connect(self):
@@ -45,11 +48,14 @@ class BasicBitsoListener(Listener):
 if __name__ == '__main__':
     api = Api()
     # api = Api(key, secret, timeout)
-
-    # btc = api.ticker("btc_usd")
-    # print(btc.ask)
-    # print(btc.bid)
+    trades = api.trades("btc_usd", marker=None, limit=1, sort='desc')
+    timezone = trades[0].created_at.tzinfo
+    today = datetime.now(timezone)
+    print(today)
+    # today_utc = today.replace(tzinfo = utc)
+    # print(today_utc)
     volume_mxn = 0
+    78488557
     tickers = ["btc_usd"]
     for ticker in tickers:
         done = False
@@ -60,17 +66,23 @@ if __name__ == '__main__':
                 volume_mxn -= trades[0].price * trade.amount
 
             for trade in trades:
+                if trade.created_at < today 
                 #some logic about if trade.created_at < current date - 30 days ago then we break
                 #set done = True
+
                 volume_mxn += trade.price * trade.amount
             print(volume_mxn)
             cur_marker = trades[-1].tid
 
-    # trades = api.trades("btc_usd", marker=None, limit=100, sort='desc')
-    # trade = trades[1]
-    # volume_mxn += trade.price * trade.amount
-    # print(volume_mxn)
-    
+    trades = api.trades("btc_usd", marker=None, limit=100, sort='desc')
+    # print(trades)
+    trade = trades[1]
+    print(trade)
+    volume_mxn += trade.price * trade.amount
+    # trade_time = trade.created_at.replace(tzinfo = utc)
+    print(trade.created_at)
+    print(today > trade.created_at)
+
     # listener = BasicBitsoListener()
     # client = Client(listener)
     # channels = ['orders']
